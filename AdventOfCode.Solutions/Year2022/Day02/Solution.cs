4352d24bf25f1b8234d00f09d1f8bb869b791f18
RoseMarieW <Rose.Wilson@tcgplayer.com>
2022-12-02 07:32:35 -0500

    Day2
    - if statements to switch
    - foreach to LINQ

--- AdventOfCode.Solutions/Year2022/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day02/Solution.cs
@@ -7,110 +7,86 @@ class Solution : SolutionBase
     protected override string SolvePartOne()
     {
         var rounds = Input.SplitByNewline();
-        var totalScore = 0;
-        foreach (var round in rounds)
+        var totalScore = rounds.Sum(round => round[2] switch
         {
-            var score = 0;
             //rock
-            if (round[2] == 'X')
+            'X' => 1 + round[0] switch
             {
-                score = 1 + round[0] switch
-                {
-                    //rock
-                    'A' => 3,
-                    //paper
-                    'B' => 0,
-                    //scissors
-                    'C' => 6,
-                    _ => 0
-                };
-            }
+                //rock
+                'A' => 3,
+                //paper
+                'B' => 0,
+                //scissors
+                'C' => 6,
+                _ => 0
+            },
             //paper
-            else if (round[2] == 'Y')
+            'Y' => 2 + round[0] switch
             {
-                score = 2 + round[0] switch
-                {
-                    //rock
-                    'A' => 6,
-                    //paper
-                    'B' => 3,
-                    //scissors
-                    'C' => 0,
-                    _ => 0
-                };
-            }
+                //rock
+                'A' => 6,
+                //paper
+                'B' => 3,
+                //scissors
+                'C' => 0,
+                _ => 0
+            },
             //scissors
-            else if (round[2] == 'Z')
+            'Z' => 3 + round[0] switch
             {
-                score = 3 + round[0] switch
-                {
-                    //rock
-                    'A' => 0,
-                    //paper
-                    'B' => 6,
-                    //scissors
-                    'C' => 3,
-                    _ => 0
-                };
-            }
-
-            totalScore += score;
-        }
+                //rock
+                'A' => 0,
+                //paper
+                'B' => 6,
+                //scissors
+                'C' => 3,
+                _ => 0
+            },
+            _ => 0
+        });
         return totalScore.ToString();
     }
 
     protected override string SolvePartTwo()
     {
         var rounds = Input.SplitByNewline();
-        var totalScore = 0;
-        foreach (var round in rounds)
+        var totalScore = rounds.Sum(round => round[2] switch
         {
-            var score = 0;
             //loose
-            if (round[2] == 'X')
+            'X' => 0 + round[0] switch
             {
-                score = 0 + round[0] switch
-                {
-                    //rock
-                    'A' => 3,
-                    //paper
-                    'B' => 1,
-                    //scissors
-                    'C' => 2,
-                    _ => 0
-                };
-            }
+                //rock
+                'A' => 3,
+                //paper
+                'B' => 1,
+                //scissors
+                'C' => 2,
+                _ => 0
+            },
             //draw
-            else if (round[2] == 'Y')
+            'Y' => 3 + round[0] switch
             {
-                score = 3 + round[0] switch
-                {
-                    //rock
-                    'A' => 1,
-                    //paper
-                    'B' => 2,
-                    //scissors
-                    'C' => 3,
-                    _ => 0
-                };
-            }
+                //rock
+                'A' => 1,
+                //paper
+                'B' => 2,
+                //scissors
+                'C' => 3,
+                _ => 0
+            },
             //win
-            else if (round[2] == 'Z')
+            'Z' => 6 + round[0] switch
             {
-                score = 6 + round[0] switch
-                {
-                    //rock
-                    'A' => 2,
-                    //paper
-                    'B' => 3,
-                    //scissors
-                    'C' => 1,
-                    _ => 0
-                };
-            }
-
-            totalScore += score;
-        }
+                //rock
+                'A' => 2,
+                //paper
+                'B' => 3,
+                //scissors
+                'C' => 1,
+                _ => 0
+            },
+            _ => 0
+        });
         return totalScore.ToString();
     }
 
